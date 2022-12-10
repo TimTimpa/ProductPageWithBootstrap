20c04ceba6c7eb85389311d59de8f46d95406734
Tim Ahlqvist <106665267+TimTimpa@users.noreply.github.com>
2022-12-10 19:23:31 +0100

    Added project

---
+++ teknikutveckling/home.js
@@ -0,0 +1,30 @@
+/*Function for scroll animation. */
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+  
+  window.addEventListener("scroll", reveal);
+
+  window.onscroll = function() {scrollFunction()};
+
+/*Navbar animation*/
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+}
+
